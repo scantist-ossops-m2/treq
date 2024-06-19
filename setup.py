dc69b890132f71bd79354cb0ae79ca086160be43
Glyph <code@glyph.im>
2024-06-19 00:57:21 -0700

    ugh this is gross, but I guess it's what you need

--- setup.py
+++
@@ -1,9 +0,0 @@
-
-from setuptools import setup
-
-# Per setuptools' documentation, incremental's plugin for setting the version
-# is only accessible if an imperative setup.py is present.
-
-# https://setuptools.pypa.io/en/latest/userguide/extension.html#adding-arguments
-
-setup(use_incremental=True)
